0632691ac853f9c4523724fede0e6b45fb943032
sockheadrps <39883642+sockheadrps@users.noreply.github.com>
2022-08-04 12:57:22 -0400

    Add files via upload
    Initial commit of working project

---
+++ main.py
@@ -0,0 +1,83 @@
+from fastapi import (
+    FastAPI,
+    Request,
+    HTTPException,
+    WebSocket,
+    WebSocketDisconnect,
+)
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from ConnectionManager import Manager
+from stats import Computer
+import logging
+import typing
+import uvicorn
+import uuid
+
+
+app = FastAPI()
+connection_manager = Manager()
+templates = Jinja2Templates(directory="templates")
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+logging.basicConfig(filename='logs.log', filemode='w', level=logging.DEBUG)
+
+
+def generate_id() -> str:
+    """
+    Generates a UUID string for unique client IDs
+    :return: String representation of UUID object
+    """
+    return str(uuid.uuid4())
+
+
+@app.get("/favicon.ico")
+async def favicon() -> typing.NoReturn:
+    # No current FavIcon - fix later
+    raise HTTPException(status_code=403, detail="No favicon")
+
+
+@app.get("/stats", response_class=HTMLResponse)
+def stats_endpoint(request: Request) -> templates.TemplateResponse:
+    """
+    HTTP endpoint to serve the Server Statistics Dashboard
+    :param request: HTTP Request from Client
+    :return: Returns the associated HTML files to the requesting client
+    """
+    client_id = generate_id()
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "id": client_id}
+    )
+
+
+@app.websocket("/ws/stats")
+async def stats_websocket(client_websocket: WebSocket):
+    """
+    Web Socket endpoint for communicating the "Server Statistics" in JSON to the client. Communication with the
+    client is done here.
+    :param client_websocket: Incoming Web Socket request
+    :return: No explicit return, just continous requests for information from client
+    """
+    await connection_manager.connect(client_websocket)
+    try:
+        await client_websocket.send_json({"event": "CONNECT"})
+        while True:
+            try:
+                data = await client_websocket.receive_json()
+                if data:
+                    logging.debug(data)
+                    if data['event']:
+                        logging.debug(f" data['event'] = {data['event']}")
+                        if data['event'] == 'DATAREQUEST':
+                            await client_websocket.send_json({"event": "DATAREQUEST",
+                                                              "data": Computer.get_stats_dict()})
+            except WebSocketDisconnect:
+                await connection_manager.disconnect_websocket(client_websocket)
+            except Exception as e:
+                logging.debug(e)
+    except Exception as e:
+        logging.debug(e)
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
---
+++ static/scripts.js
@@ -0,0 +1,208 @@
+// Open Websocket connection
+var socket = new WebSocket("ws://localhost:8080/ws/stats");
+
+// On open function
+socket.onopen = function(event) {
+    socket.send(JSON.stringify({event: "DATAREQUEST"}));
+};
+
+
+// Setting up HTML Elements
+let cpu_count = document.getElementById("cpu_count")
+let cpu_usage = document.getElementById("cpu_usage")
+let cpu_frequency = document.getElementById("cpu_frequency")
+let core_temperature = document.getElementById("core_temperature")
+let ram_total = document.getElementById("ram_total")
+let ram_availble = document.getElementById("ram_available")
+let ram_percentage = document.getElementById("ram_percentage")
+let disk_total = document.getElementById("disk_total")
+let disk_free = document.getElementById("disk_free")
+let disk_used = document.getElementById("disk_used")
+let disk_percentage = document.getElementById("disk_percentage")
+
+
+// Main Websocket Communication
+socket.onmessage = function(event) {
+    let data = JSON.parse(event.data);
+    if (data.data){
+        updateData(data.data)
+    }
+};
+
+// Interval Timer to request Stats
+setInterval(requestTimer, 1000);
+function requestTimer() {
+  socket.send(JSON.stringify({event: "DATAREQUEST"}));
+}
+
+
+// Chart configs
+let updateInterval = 20 //in ms
+let numberElements = 200;
+
+//Globals
+let updateCount = 0;
+
+
+// Chart Objects
+let cpuUsageChart = document.getElementById("cpuUsage");
+let ramUsageChart = document.getElementById("ramUsage");
+let diskUsageChart = document.getElementById("diskUsage");
+
+
+// Common Chart Options (Line)
+let commonOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    backgroundColor: "rgba(255,99,132,0.2)",
+    borderColor: "rgba(255,99,132,1)",
+    fill: true,
+    scales: {
+      x: {
+        color: "black",
+        grid: {
+            display: false
+        },
+        ticks: {
+            color: "black"
+        },
+      },
+      y: {
+        beginAtZero: true,
+        max: 100,
+        grid: {
+            color: "grey"
+        },
+        ticks: {
+            color: "black"
+        },
+      }
+    },
+    legend: {display: false},
+    tooltips:{
+      enabled: false
+    }
+};
+
+
+// cpuUsageChart Instance
+var cpuUsageChartInstance = new Chart(cpuUsageChart, {
+    type: 'line',
+    data: {
+      datasets: [{
+          label: "CPU Usage",
+          data: 0,
+          borderWidth: 1
+      }]
+    },
+    options: Object.assign({}, commonOptions, {
+    responsive: true,
+      title:{
+        display: true,
+        text: "CPU Usage",
+        fontSize: 18
+      }
+    })
+});
+
+// ramUsageChart Instance
+var ramUsageChartInstance = new Chart(ramUsageChart, {
+    type: 'line',
+    data: {
+      datasets: [{
+          label: "RAM Usage",
+          data: 0,
+          borderWidth: 1
+      }]
+    },
+    options: Object.assign({}, commonOptions, {
+      title:{
+        display: true,
+        text: "RAM Usage",
+        fontSize: 18
+      }
+    })
+});
+
+// diskUsageChart Instance
+var diskUsageChartInstance = new Chart(diskUsageChart, {
+    type: 'doughnut',
+    labels: [
+        'free',
+        'Used'
+    ],
+    data: {
+      datasets: [{
+          label: "Disk Usage",
+          data: [1, 1],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+          ],
+          hoverOffset: 4
+      }]
+    },
+    options: Object.assign({}, {
+      title:{
+        display: true,
+        text: "Disk Usage",
+        fontSize: 18
+      }
+    })
+});
+
+// Function to push data to chart object instances
+function addData(data) {
+    if(data){
+        let today = new Date();
+        let time
+        if (today.getMinutes < 10){
+            time = today.getHours() + ":0" + today.getMinutes();
+        }else{
+            time = today.getHours() + ":" + today.getMinutes();
+        }
+        // CPU Usage
+        cpuUsageChartInstance.data.labels.push(time);
+        cpuUsageChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data.cpu_usage)});
+        // RAM Usage
+        ramUsageChartInstance.data.labels.push(time);
+        ramUsageChartInstance.data.datasets.forEach((dataset) =>{dataset.data.push(data.ram_percentage)});
+        // Disk Usage
+        diskUsageChartInstance.data.datasets[0].data[0] = data.disk_used;
+        diskUsageChartInstance.data.datasets[0].data[1] = data.disk_free;
+
+
+      if(updateCount > numberElements){
+        // For shifting the x axis markers
+        // CPU Usage
+        cpuUsageChartInstance.data.labels.shift();
+        cpuUsageChartInstance.data.datasets[0].data.shift();
+        // RAM Usage
+        ramUsageChartInstance.data.labels.shift();
+        ramUsageChartInstance.data.datasets[0].data.shift();
+
+      }
+      else updateCount++;
+      cpuUsageChartInstance.update();
+      ramUsageChartInstance.update();
+      diskUsageChartInstance.update();
+
+    }
+  };
+
+  // Update HTML elements
+function updateData(data) {
+    addData(data)
+    cpu_count.innerHTML = "Core count: " + data.cpu_count.toString()
+    cpu_usage.innerHTML = "CPU usage: " + data.cpu_usage.toString() + "%"
+    cpu_frequency.innerHTML = "CPU Frequency: " + data.cpu_frequency.current_frequency.toString() + " GHz"
+    ram_total.innerHTML = "RAM total: " +  data.ram_total.toString() + " GB"
+    ram_available.innerHTML = "RAM Available: " + data.ram_available.toString() + " GB"
+    ram_percentage.innerHTML = "Percentage of RAM used: " + data.ram_percentage.toString() + "%"
+    disk_total.innerHTML = "Disk space total: " + data.disk_total.toString() + " GB"
+    disk_free.innerHTML = "Disk Space Free: " + data.disk_free.toString() + " GB"
+    disk_used.innerHTML = "Disk Space used: " + data.disk_used.toString() + " GB"
+    disk_percentage = "Disk Space Used: "+ data.disk_percentage.toString() + "%"
+
+}
+updateData()
