49ab40f5ba106732cbec6551c4d70ceab4c40aae
BurningSquid <austin.r.bailey1@gmail.com>
2022-08-06 20:00:51 -0700

    connection manager name

--- src/server_stats/connection_manager.py
+++ src/server_stats/connection_manager.py
@@ -2,13 +2,14 @@ from fastapi import WebSocket
 from typing import List, NoReturn
 
 
-class Manager:
-	def __init__(self):
-		self.connections: List[WebSocket] = []
+# REVIEW: classes should be capitalized and named the same as the file
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
 
-	async def connect(self, web_socket: WebSocket) -> NoReturn:
-		await web_socket.accept()
-		self.connections.append(web_socket)
+    async def connect(self, web_socket: WebSocket) -> NoReturn:
+        await web_socket.accept()
+        self.connections.append(web_socket)
 
-	async def disconnect_websocket(self, web_socket: WebSocket) -> NoReturn:
-		self.connections.remove(web_socket)
+    async def disconnect_websocket(self, web_socket: WebSocket) -> NoReturn:
+        self.connections.remove(web_socket)
