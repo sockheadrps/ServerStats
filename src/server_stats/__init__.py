4ce6d2c7cfa331ec33eca119f0a8d405b6d725f6
Austin Bailey <austin.r.bailey1@gmail.com>
2022-08-07 10:19:05 -0700

    comments

--- src/server_stats/__init__.py
+++ src/server_stats/__init__.py
@@ -1,5 +1,6 @@
-# in order to be recognized as a module must have a __init__.py file
-# https://docs.python.org/3.8/library/__main__.html
+# REVIEW: in order to be recognized as a module must have a __init__.py file
+# https://docs.python.org/3.8/library/__main__.html. Module can now be run with python -m "module name"
+
 import logging
 
 from .constants import LOGGING_DIR
