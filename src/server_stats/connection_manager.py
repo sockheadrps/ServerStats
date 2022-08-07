522b4505910f64ec14a7b48d89c71d22ed47c302
BurningSquid <austin.r.bailey1@gmail.com>
2022-08-06 23:34:23 -0700

    refactor connection manager

--- src/server_stats/connection_manager.py
+++ src/server_stats/connection_manager.py
@@ -1,5 +1,5 @@
 from fastapi import WebSocket
-from typing import List, NoReturn
+from typing import List
 
 
 # REVIEW: classes should be capitalized and named the same as the file
@@ -7,9 +7,9 @@ class ConnectionManager:
     def __init__(self):
         self.connections: List[WebSocket] = []
 
-    async def connect(self, web_socket: WebSocket) -> NoReturn:
+    async def connect(self, web_socket: WebSocket) -> None:
         await web_socket.accept()
         self.connections.append(web_socket)
 
-    async def disconnect_websocket(self, web_socket: WebSocket) -> NoReturn:
+    async def disconnect_websocket(self, web_socket: WebSocket) -> None:
         self.connections.remove(web_socket)
