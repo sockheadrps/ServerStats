0180dec6f2e44114729583c286940f20f3ce6118
BurningSquid <austin.r.bailey1@gmail.com>
2022-08-06 23:34:14 -0700

    refactor main WIP

--- src/server_stats/__main__.py
+++ src/server_stats/__main__.py
@@ -1,33 +1,37 @@
-from fastapi import (
-    FastAPI,
-    Request,
-    HTTPException,
-    WebSocket,
-    WebSocketDisconnect,
-)
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from .connection_manager import Manager
-from .stats import Computer
+# REVIEW: imports should be sorted in groups: top is python standard library, middle is third party, bottom is local
 import logging
-import typing
-import uvicorn
 import uuid
 
-# the __main__.py (called a dunder main) is the top-level script that is executed when the program is run
+import uvicorn
+from fastapi import FastAPI, HTTPException, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# REVIEW: all internal imports should be relative
+from .connection_manager import ConnectionManager
+from .constants import STATIC_DIR
+from .stats import Computer
+
+# REVIEW: best practice for logger is to initialize logger settings in __ini__.py
+# then get the logger when being used in a script. __name__ is going to return main which was
+# the name of the logger
+logger = logging.getLogger(__name__)
+
+# REVIEW: the __main__.py (called a dunder main) is the top-level script that is executed when the program is run
 # https://docs.python.org/3.8/library/__main__.html
 
+# REVIEW: good to have a separate function for main and
+# call it in the if statement below
 app = FastAPI()
-connection_manager = Manager()
+connection_manager = ConnectionManager()
 templates = Jinja2Templates(directory="templates")
 app.mount(
     "/static",
-    StaticFiles(directory=r"C:\Users\rpski\Desktop\Example Code\ServStats\static"),
+    # REVIEW: file paths should be constants in most cases
+    StaticFiles(directory=STATIC_DIR),
     name="static",
 )
-
-logging.basicConfig(filename="./logs/logs.log", filemode="w", level=logging.DEBUG)
 
 
 def generate_id() -> str:
@@ -38,14 +42,16 @@ def generate_id() -> str:
     return str(uuid.uuid4())
 
 
+# REVIEW: not really needed to type NoRetur, can just have no type hint
+# or type hint None if you really feel like it
 @app.get("/favicon.ico")
-async def favicon() -> typing.NoReturn:
+async def favicon() -> None:
     # No current FavIcon - fix later
     raise HTTPException(status_code=403, detail="No favicon")
 
 
 @app.get("/stats", response_class=HTMLResponse)
-def stats_endpoint(request: Request) -> templates.TemplateResponse:
+def stats_endpoint(request: Request) -> HTMLResponse:
     """
     HTTP endpoint to serve the Server Statistics Dashboard
     :param request: HTTP Request from Client
@@ -80,14 +86,15 @@ async def stats_websocket(client_websocket: WebSocket):
                     )
                 else:
                     # Log if for some reason we get unexpected communication protocol from the client
-                    logging.debug(data)
+                    # REVIEW: want to log to a specific logger rather than all loggers
+                    logger.debug(data)
             except WebSocketDisconnect:
                 await connection_manager.disconnect_websocket(client_websocket)
                 break
             except Exception as e:
-                logging.debug(e)
+                logger.debug(e)
     except Exception as e:
-        logging.debug(e)
+        logger.debug(e)
 
 
 if __name__ == "__main__":
