9d83413b2990d8212cc26239f1d258dddbdb242d
Austin Bailey <austin.r.bailey1@gmail.com>
2022-08-07 10:44:22 -0700

    .env file

--- src/server_stats/constants.py
+++ src/server_stats/constants.py
@@ -1,3 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# REVIEW: things like port and local host should be saved as ENV variables
+PORT = int(os.environ.get("PORT"))
+HOST = os.environ.get("HOST")
+
 # REVIEW: all constants should be in all caps.
 # REVIEW: filepaths should be relative unless set a the environment level
 STATIC_DIR = "static"
