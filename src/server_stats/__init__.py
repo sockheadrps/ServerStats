63bbc564d270d427dd2a63fe34402b0913351b95
BurningSquid <austin.r.bailey1@gmail.com>
2022-08-06 23:11:44 -0700

    logging in init

--- src/server_stats/__init__.py
+++ src/server_stats/__init__.py
@@ -1,2 +1,7 @@
 # in order to be recognized as a module must have a __init__.py file
 # https://docs.python.org/3.8/library/__main__.html
+import logging
+
+from .constants import LOGGING_DIR
+
+logging.basicConfig(filename=LOGGING_DIR, filemode="w", level=logging.DEBUG)
